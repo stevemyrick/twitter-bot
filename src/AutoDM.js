6dbbc5421242f84d13537b863a4e84c4ac181f82
stevemyrick <stevemyrick513@gmail.com>
2019-05-31 12:18:17 -0400

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -16,7 +16,7 @@ const SendMessage = user => {
     text: GenerateMessage(name)
   };
   // the follow stream track if I follow author person too.
-  if (screen_name != my_user_name) {
+  if (screen_name != my_user_name && screen_name screen_name.includes("gl00m")) {
     console.log(" 🎉🎉🎉🎉 New Follower  🎉🎉🎉🎉🎉 ");
     setTimeout(() => {
       T.post("direct_messages/new", obj)
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Hey fellow hacker, have a flag: 1mp3rs0nat1ng_a_fr13nd`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
